aba5aa898eff298284242e14d6a08199b238f972
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-05-22 13:15:07 +0300

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
   $('.big__slider').slick();
+  $('.nav__slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    arrows: false,
+    slidesToScroll: 2
+  });
+  $('.special__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    arrows: true,
+    slidesToScroll: 1
+  });
+  $('.article__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    arrows: true,
+    slidesToScroll: 1
+  });
+  
 })
