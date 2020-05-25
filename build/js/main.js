149e125055e7f8d00cda5e5f583212cf94fc226d
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-05-25 14:04:39 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -10,13 +10,51 @@ $(function(){
     infinite: true,
     slidesToShow: 4,
     arrows: true,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
   $('.article__slider').slick({
     infinite: true,
     slidesToShow: 4,
     arrows: true,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
+  if($(window).width() < 993){
+    $('.left__menu nav').append( $('.header__contact') );
+
+    $('.burger').click(function(){
+      $(this).toggleClass('active')
+      $('.left__menu__item').removeClass('open');
+      $('.left__menu').fadeToggle(0)
+      $('.left__menu').toggleClass('active')
+    })  
+
+    $('.left__menu__item').click(function(){
+      $(this).toggleClass('open')
+    })
+    
+  }
+  if($(window).width() < 768){
+    $('.left__menu nav').append( $('.header__nav nav') );
+  }
+  
   
 })
+
